c2b8bdd2bf9a870427b2e20afdcaf49c5333b4df
Kanwal Dhindsa <kandhind@duosecurity.com>
2022-05-12 05:48:47 -0700

    removed syft step

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,10 +3,3 @@
 def call(String name = 'human') {
   echo "Hello, ${name}."
 }
-
-def cmd = """curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin"""
-def process = command.execute()
-def sOut = new StringBuilder()
-def sErr = new StringBuilder()
-process.consumeProcessOutput(sOut,sErr)
-process.waitForOrKill(60000) //milliseconds... timeout
